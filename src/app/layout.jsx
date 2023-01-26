1438402ecfcb51f06da21a38130f1344ad74c1fe
Zehra İkizler <zehraaikizler@gmail.com>
2023-01-26 15:41:00 +0300

    project created

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,10 @@
-import './globals.css'
+import "@/styles/reset.css";
+import "@/styles/global.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html lang="tr">
       <body>{children}</body>
     </html>
-  )
+  );
 }
