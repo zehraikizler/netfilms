4101784134416d3267b0adf2dbb31b775957ef90
Zehra İkizler <zehraaikizler@gmail.com>
2023-01-26 16:11:35 +0300

    header and footer comp added

---
+++ src/components/footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Made with ❤️ by&nbsp;
+      <Link
+        href="https://www.linkedin.com/in/zehra-ikizler-831573208/"
+        target="_blank"
+      >
+        Zehra İkizler
+      </Link>
+    </footer>
+  );
+}
+
+export { Footer };
