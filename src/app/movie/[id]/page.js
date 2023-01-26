675f6866f3d4c8ab1e314095495e4c1daaee39b6
Zehra İkizler <zehraaikizler@gmail.com>
2023-01-26 17:10:27 +0300

    movie id added

---
+++ src/app/movie/[id]/page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MovieContainer from "@/containers/movie";
+import Movies from "@/mocks/movies.json";
+import { notFound } from "next/navigation";
+
+function MoviePage({ params, searchParams }) {
+  const movieDetail = Movies.results.find(
+    (movie) => movie.id.toString() === params.id
+  );
+
+  if (!movieDetail) {
+    notFound();
+  }
+
+  if (searchParams.error == "true") {
+    throw new Error("Error happened");
+  }
+
+  return <MovieContainer movie={movieDetail} />;
+}
+
+export default MoviePage;
